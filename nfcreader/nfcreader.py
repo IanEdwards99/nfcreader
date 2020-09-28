4104b5024fe8389ec819fa613b0426c0140ad321
Agi23 <Agoritsa.spirakis@gmail.com>
2020-09-28 21:24:18 +0200

    hello world code added

--- nfcreader/nfcreader.py
+++ nfcreader/nfcreader.py
@@ -1,1 +1,13 @@
 """Main module."""
+from flask import Flask
+app = Flask(__name__)
+@app.route ('/')
+
+def hello_world():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run()
+
+#def insertReading(data)
+
